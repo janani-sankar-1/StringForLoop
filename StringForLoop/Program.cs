47d732439bc5f703aaabcbcd32e096295e5d01e6
Janani S <Janani S@DESKTOP-13MT9AS>
2024-10-15 19:29:14 +0530

    Add project files.

---
+++ StringForLoop/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StringForLoop
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "developer@example.com";
+            char[] vowels = new char[] { 'A', 'E', 'I', 'O', 'U', 'a', 'e', 'i', 'o', 'u' };
+
+            int vowelsCount = 0;
+            for (int i = 0; i < name.Length; i++)
+            {
+                bool isMatch = false;
+                for (int j = 0; j < vowels.Length; j++)
+                    if (name[i] == vowels[j])
+                        isMatch = true;
+
+                if (isMatch)
+                {
+                    vowelsCount++; 
+                }
+                Console.WriteLine(name[i] + " " + isMatch);
+            }
+            Console.WriteLine(vowelsCount + " vowels found");
+
+            //Another way for loop
+            int vowelsCount2 = 0;
+            for (int u = 0; u < name.Length; u++)
+            {
+                if (Array.IndexOf(vowels, name[u]) != -1)
+                {
+                    vowelsCount2++;
+                }
+            }
+            Console.WriteLine(vowelsCount2 + " vowels found");
+
+            // same using LINQ
+            Console.WriteLine(name.Count(ch=>Array.IndexOf(vowels,ch)>=0));
+
+            Console.ReadKey();
+        }
+    }
+}
